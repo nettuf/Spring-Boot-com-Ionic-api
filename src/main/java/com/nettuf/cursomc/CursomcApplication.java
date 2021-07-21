cb41eec52aa02bf2e8310c64e1060213b3d48bed
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-21 15:12:16 -0300

    categoria resource created

--- src/main/java/com/nettuf/cursomc/CursomcApplication.java
+++ src/main/java/com/nettuf/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.nettuf.workshopmongo;
+package com.nettuf.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
