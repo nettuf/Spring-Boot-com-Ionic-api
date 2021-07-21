0757c5345f39249b064a1d16fb51fc890e89951a
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-21 17:42:32 -0300

    adding categoria class domain

--- src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.nettuf.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nettuf.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista;
 	}
 	
 }
