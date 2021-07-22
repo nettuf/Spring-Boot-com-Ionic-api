a9522eab0cabf2a33fe7b964823ffbb94218a90a
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-22 18:19:58 -0300

    adjust in the endpoint /categorias /id and create personal exception

--- src/main/java/com/nettuf/cursomc/domain/Produto.java
+++ src/main/java/com/nettuf/cursomc/domain/Produto.java
@@ -13,6 +13,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -23,6 +25,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 	joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/nettuf/cursomc/services/CategoriaService.java
+++ src/main/java/com/nettuf/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.nettuf.cursomc.domain.Categoria;
 import com.nettuf.cursomc.repositories.CategoriaRepository;
+import com.nettuf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Object not found! id:"+id+", tipo:"+Categoria.class.getName()));
 	}
 }
