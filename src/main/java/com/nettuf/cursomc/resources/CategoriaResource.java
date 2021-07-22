1d332bce43b6680c33c2c18a1db2c15e43798df8
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-22 15:19:56 -0300

    creating repository and service to category

--- src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,26 @@
 package com.nettuf.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nettuf.cursomc.domain.Categoria;
+import com.nettuf.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.addAll(Arrays.asList(cat1, cat2));
-		
-		return lista;
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
