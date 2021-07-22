2dc35eea36aa0d56b4f02452eb7890d20ff9470d
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-22 15:47:27 -0300

    creating operation of instantiation

--- src/main/java/com/nettuf/cursomc/CursomcApplication.java
+++ src/main/java/com/nettuf/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.nettuf.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nettuf.cursomc.domain.Categoria;
+import com.nettuf.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
