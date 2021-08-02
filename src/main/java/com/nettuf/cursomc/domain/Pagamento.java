4fbb7566855712f40720145c28aa54ad8e487739
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-08-02 14:57:08 -0300

    pedido-pagamento mapping one to one

--- src/main/java/com/nettuf/cursomc/domain/Pagamento.java
+++ src/main/java/com/nettuf/cursomc/domain/Pagamento.java
@@ -3,14 +3,25 @@ package com.nettuf.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.nettuf.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name = "pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
--- src/main/java/com/nettuf/cursomc/domain/Pedido.java
+++ src/main/java/com/nettuf/cursomc/domain/Pedido.java
@@ -4,11 +4,23 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
+	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	
 	private Pagamento pagamento;
 	
