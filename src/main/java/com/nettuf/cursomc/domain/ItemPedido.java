8bc945b7bb41c6170e837fc27ef584028a8c02f0
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-08-03 13:33:09 -0300

    itempedido repository and dependencies

--- src/main/java/com/nettuf/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nettuf/cursomc/domain/ItemPedido.java
@@ -3,9 +3,14 @@ package com.nettuf.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
 	private Double desconto;
--- src/main/java/com/nettuf/cursomc/domain/Produto.java
+++ src/main/java/com/nettuf/cursomc/domain/Produto.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -34,6 +35,7 @@ public class Produto implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
 
 	
